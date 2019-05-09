5bf6c735f49757bb6e2ff50ae91d1a637eab0872
Etimreht <36156395+Etimreht@users.noreply.github.com>
2019-05-09 18:11:11 +0100

    A* setup beginning

---
+++ AI/Assets/Script/AI/Pathfinding/AStar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStar : MonoBehaviour
+{
+    Grid grid;
+
+    private void Awake()
+    {
+        grid = GetComponent<Grid>();
+    }
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        List<Node> openset = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+        openset.Add(startNode);
+
+        while (openset.Count > 0)
+        {
+            Node currentNode = openset[0];
+            for (int i = 1; i < openset.Count; i++)
+            {
+                if (openset[i].fCost < currentNode.fCost || openset[i].fCost == currentNode.fCost && openset[i].hCost < currentNode.hCost)
+                {
+                    currentNode = openset[i];
+                }
+            }
+            openset.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            if (currentNode == targetNode)
+            {
+                return;
+            }
+
+        }
+    }
+
+
+
+
+
+
+}
--- AI/Assets/Script/AI/Pathfinding/Node.cs
+++ AI/Assets/Script/AI/Pathfinding/Node.cs
@@ -7,10 +7,19 @@ public class Node
     public bool walkable;
     public Vector3 WorldPosition;
 
+    public int gCost, hCost;
+
     public Node(bool _walkable, Vector3 _worldposition)
     {
         walkable = _walkable;
         WorldPosition = _worldposition;
     }
 
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+    }
 }
